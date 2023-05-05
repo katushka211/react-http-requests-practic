7e520fc6aed384581134fde54e8ec439acbf3dde
Kateryna Verveda <katushka211@gmail.com>
2023-05-05 16:36:32 +0200

    add APP with fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import { ToastContainer } from 'react-toastify';
+
+export class App extends Component {
+  state = {
+    pokemon: null,
+  };
+
+  async componentDidMount() {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(res => res.json())
+      .then(pokemon => this.setState({ pokemon }));
+  }
+
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        {this.state.pokemon && <div>{this.state.pokemon.name}</div>}
+      </div>
+    );
+  }
+}
